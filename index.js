90ca4d21842c877c6ef2a9a65a8019d60a9e5fe5
Hichem <hichembouzourine.uni@gmail.com>
2022-08-20 05:36:55 +0100

    Revert "modify origin url."
    This reverts commit 255e3f3df3a788086036c3d0a02fb97c24cfdfa9.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://vidly-zone.herokuapp.com/",
+  origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
