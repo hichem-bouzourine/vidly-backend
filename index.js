3f26c9fbaab15f5f62d2a406a5e4ec62f50f2863
Hichem <hichembouzourine.uni@gmail.com>
2022-08-20 05:29:06 +0100

    Add url as a env variable.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const config = require("config");
 const winston = require("./startup/logging");
 const express = require("express");
 const app = express();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: config.get("url"),
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
